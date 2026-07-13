736d06aafe484fc2dff964f20487ef24a89f1e3d
Kiran <kiran.sadara6730@gmail.com>
2026-07-13 22:17:47 +0530

    Adding the logger

---
+++ backend/dist/logging/logger.js
@@ -0,0 +1,24 @@
+import winston from 'winston';
+import path from 'path';
+import fs from 'fs';
+const date = new Date().toISOString().split("T")[0];
+const logsDir = path.join(process.cwd(), "src", "logs");
+if (!fs.existsSync(logsDir)) {
+    fs.mkdirSync(logsDir, { recursive: true });
+}
+const logger = winston.createLogger({
+    level: "info",
+    format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new winston.transports.File({
+            filename: path.join(logsDir, "error.log"),
+            level: "error"
+        }),
+        new winston.transports.File({
+            filename: path.join(logsDir, `backend_${date}.log`)
+        })
+    ]
+});
+export default logger;
+//# sourceMappingURL=logger.js.map
